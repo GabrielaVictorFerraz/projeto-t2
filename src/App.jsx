1ed72429d053f64c61de97ca5d2f4dde758b9ec1
gabrielavictorferraz321 <gabriela.victor.ferraz321@gmail.com>
2025-09-26 21:17:25 -0300

    Componentes e JSX / Props e Children (FORMULARIO)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { Hello } from "./components/Hello";
+import { Card } from "./components/Card";
+
+const CARDS = [
+  { id: 0, title: 'Card A', description: 'Filho de A' }, 
+  { id: 1, title: 'Card B', description: 'Filho de B' },
+  { id: 2, title: 'Card C', description: 'Filho de C' },
+]
+
 const App = () => {
   return (
     <main className="min-h-dvh grid place-items-center bg-amber-50">
@@ -6,14 +15,16 @@ const App = () => {
       </h1>
 
       <div className="container mx-auto p-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <div className="rounded-2x1 border p-4 bg-slate-400 text-neutral-50 shadow-sm">Card A</div>
-        <div className="rounded-2x1 border p-4 bg-slate-400 text-neutral-50 shadow-sm">Card B</div>
-        <div className="rounded-2x1 border p-4 bg-slate-400 text-neutral-50 shadow-sm">Card C</div>
+        {CARDS.map(item => (
+          <Card key={item.id} title={item.title}>
+            <p>{item.description}</p>
+            </Card>
+        ))}
       </div>
-      <a href="#" className="inline-block px-4 py-2 rounded-lg border hover:bg-slate-100 focus:ring-2 focust:ring-blue-500">
+      <a href="#" className="inline-block px-4 py-2 rounded-lg border hover:bg-slate-100 focus:ring-2 focust: ring-blue-500">
         Link com foco
       </a>
-
+        <Hello name="Gabriela" />
     </main>
   );
 }
